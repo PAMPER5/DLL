1ec060f2295d717b4728335884b07b320fa6dd37
PAMPER5 <uglanovmurad1999@gmail.com>
2024-02-15 22:22:27 +0300

    Добавить экспортную функцию запуска калькулятора

---
+++ CalcDll/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CalcDll
+{
+    public class CalculatorStarter
+    {
+        [DllExport("StartCalculator", CallingConvention = CallingConvention.StdCall)]
+        public static int StartCalculator(IntPtr hwnd, IntPtr hinst, IntPtr lpszCmdLine, int nCmdShow)
+        {
+            try
+            {
+                Process.Start("calc.exe");
+                return 0; // Успешный запуск калькулятора  
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при запуске калькулятора: " + ex.Message);
+                return 1; // Ошибка при запуске калькулятора  
+            }
+        }
+    }
+}
+
